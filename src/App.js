95211cd94a4059427761710e35d0e0cc2088f113
Kalum Gunawardhana <kguna415@gmail.com>
2025-05-28 15:37:39 +0530

    commit

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import logo from './logo.svg';
 import './App.css';
-import { useState } from 'react';
+import { useEffect, useState } from 'react';
+import MovieCard from './components/MovieCard';
 
 // function App() {
 //   return (
